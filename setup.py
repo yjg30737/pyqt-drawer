c513b3229ea0c18ae19fa620877725c728bc6cdb
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 09:36:34 +0900

    Update setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-drawer',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -10,8 +17,10 @@ setup(
     package_data={'pyqt_drawer.ico': ['drawer.svg']},
     description='PyQt drawer',
     url='https://github.com/yjg30737/pyqt-drawer.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-svg-icon-pushbutton @ git+https://git@github.com/yjg30737/pyqt-svg-icon-pushbutton.git@main'
+        'pyqt-svg-icon-pushbutton>=0.0.1'
     ]
 )
