0a3fca65e1ca6e3289a442da0bc18b01f621d056
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-16 09:18:03 +0900

    Add the opacity animation, make drawer save the space

--- pyqt_drawer/drawer.py
+++ pyqt_drawer/drawer.py
@@ -1,14 +1,17 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout
+from PyQt5.QtWidgets import QWidget, QGridLayout, QGraphicsOpacityEffect
 from PyQt5.QtCore import Qt, QPropertyAnimation, QAbstractAnimation
 from pyqt_svg_icon_pushbutton import SvgIconPushButton
 
 
 class Drawer(QWidget):
-    def __init__(self, widget, orientation=Qt.Horizontal):
+    def __init__(self, parent, widget, orientation=Qt.Horizontal):
         super().__init__()
-        self.__initUi(widget=widget, orientation=orientation)
+        self.__initUi(parent, widget=widget, orientation=orientation)
 
-    def __initUi(self, widget, orientation):
+    def __initUi(self, parent, widget, orientation):
+        self.__parent = parent
+        self.__parent.installEventFilter(self)
+
         self.__btn = SvgIconPushButton()
         self.__btn.setCheckable(True)
         self.__btn.toggled.connect(self.__drawerToggled)
@@ -16,40 +19,67 @@ class Drawer(QWidget):
         self.__btn.setPadding(5)
 
         self.__widget = widget
-        self.__animation = ''
+
+        # init size animation
+        self.__sizeAnimation = ''
         if orientation == Qt.Horizontal:
             self.__widget.setFixedWidth(0)
-            self.__animation = QPropertyAnimation(self, b"width")
-            self.__animation.valueChanged.connect(self.__widget.setFixedWidth)
+            self.__sizeAnimation = QPropertyAnimation(self, b"width")
+            self.__sizeAnimation.valueChanged.connect(self.__widget.setFixedWidth)
         else:
             self.__widget.setFixedHeight(0)
-            self.__animation = QPropertyAnimation(self, b"height")
-            self.__animation.valueChanged.connect(self.__widget.setFixedHeight)
+            self.__sizeAnimation = QPropertyAnimation(self, b"height")
+            self.__sizeAnimation.valueChanged.connect(self.__widget.setFixedHeight)
+        self.__sizeAnimation.setStartValue(0)
+        self.__sizeAnimation.setDuration(200)  # default duration
+        self.__sizeAnimation.setEndValue(200)  # default end value
 
-        self.__animation.setStartValue(0)
-        self.__animation.setDuration(200) # default duration
-        self.__animation.setEndValue(200) # default end value
+        # init opacity animation
+        self.__opacityAnimation = ''
+        self.__widget.setGraphicsEffect(QGraphicsOpacityEffect(opacity=0.0))
+        self.__opacityAnimation = QPropertyAnimation(self, b"opacity")
+        self.__opacityAnimation.valueChanged.connect(self.__setOpacity)
+
+        self.__opacityAnimation.setStartValue(0.0)
+        self.__opacityAnimation.setDuration(200)
+        self.__opacityAnimation.setEndValue(1.0)
 
         lay = QGridLayout()
-        lay.addWidget(self.__widget, 1, 0, 1, 2)
         lay.addWidget(self.__btn, 0, 0, 1, 1, Qt.AlignTop | Qt.AlignLeft)
+        lay.addWidget(self.__widget, 0, 0, 1, 2)
         lay.setSpacing(0)
 
         self.setLayout(lay)
 
     def __drawerToggled(self, f):
         if f:
-            self.__animation.setDirection(QAbstractAnimation.Forward)
+            self.__sizeAnimation.setDirection(QAbstractAnimation.Forward)
+            self.__opacityAnimation.setDirection(QAbstractAnimation.Forward)
+            self.__btn.hide()
         else:
-            self.__animation.setDirection(QAbstractAnimation.Backward)
-        self.__animation.start()
+            self.__sizeAnimation.setDirection(QAbstractAnimation.Backward)
+            self.__opacityAnimation.setDirection(QAbstractAnimation.Backward)
+            self.__btn.show()
+        self.__opacityAnimation.start()
+        self.__sizeAnimation.start()
+
+    def __setOpacity(self, opacity):
+        opacity_effect = QGraphicsOpacityEffect(opacity=opacity)
+        self.__widget.setGraphicsEffect(opacity_effect)
 
     def setDuration(self, msecs):
-        self.__animation.setDuration(msecs)
+        self.__sizeAnimation.setDuration(msecs)
+        self.__opacityAnimation.setDuration(msecs)
 
     def setEndValue(self, value):
-        self.__animation.setEndValue(value)
+        self.__sizeAnimation.setEndValue(value)
 
     def setIcon(self, icon):
         self.__btn.setIcon(icon)
 
+    def eventFilter(self, obj, e):
+        if isinstance(obj, type(self.__parent)):
+            if e.type() == 3:
+                if self.__sizeAnimation.currentValue() == self.__sizeAnimation.endValue():
+                    self.__btn.toggle()
+        return super().eventFilter(obj, e)
